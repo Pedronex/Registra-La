77badd75cccdf95b880761b101370aa8c56b5d6d
Pedro Soares <pedrosoares.nex@gmail.com>
2025-10-09 10:45:58 -0300

    Bump version to 1.2.1

--- app.config.ts
+++ app.config.ts
@@ -28,7 +28,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: getAppName(),
   slug: `registra-la`,
   owner: 'nexcorp',
-  version: '1.1.0',
+  version: '1.2.1',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'registrala',
